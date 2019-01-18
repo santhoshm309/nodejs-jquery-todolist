ca2a153f259548630fd1da901423177b2490894c
Santhosh Mohan <santhoshm309@gmail.com>
2019-01-18 16:58:09 +0530

    Final

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ var bodyParser = require('body-parser');
 
 var connection = require('./app/config/connection');
 var routes = require('./app/controllers/routes');
+var logger = require('morgan');
+var cors  = require('cors');
+
 
 var app = express();
+
+app.use(cors());
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+app.use(logger('dev'));
+app.use('/', express.static(__dirname + '/client'));
 
 connection.init();
 routes.configure(app);
 
-var server = app.listen(8000, function(){
+var server = app.listen(3000, function(){
   console.log('Server listening on port ' + server.address().port);
 });
